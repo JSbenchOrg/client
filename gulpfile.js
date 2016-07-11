e1c1d78b3442dfd4641979b47822d0187e971449
Bogdan Anton <contact@bogdananton.ro>
2016-07-12 02:49:12 +0300

    Added livereload. moved public / static / generated to public.
    /js/app.min.js is removed from GIT.
    Run `gulp` to load the app.

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,11 @@ var gulp        = require('gulp');
 var browserify  = require('browserify');
 var source      = require('vinyl-source-stream');
 var tsify       = require('tsify');
+var gulp        = require('gulp');
+var server      = require('gulp-server-livereload');
 
 gulp.task('tsc', function() {
+    console.log('Transpile ts/ -> js/ ...');
     return browserify({debug: true})
         .add('ts/index.ts')
         .plugin(tsify)
@@ -12,7 +15,11 @@ gulp.task('tsc', function() {
             console.log(e.message);
         })
         .pipe(source('app.min.js'))
-        .pipe(gulp.dest('js/'));
+        .pipe(gulp.dest('public/js/'));
+});
+
+gulp.task('watch-tsc', function() {
+    gulp.watch('ts/**/*.ts', { interval: 1000 }, ['tsc'])
 });
 
 /**
@@ -31,3 +38,17 @@ gulp.task('tsc', function() {
 //    var tsResult = gulp.src('ts/**/*.ts').pipe(ts(tsProject));
 //    return tsResult.js.pipe(gulp.dest('js/tmp'));
 //});
+
+gulp.task('serve', function() {
+    gulp.src('public').pipe(server({
+        livereload: true,
+        directoryListing: false,
+        open: true,
+        port: 8000,
+        // host: 'localhost',
+        host: 'jsbench.net.serbang',
+        defaultFile: 'index.html',
+    }));
+});
+
+gulp.task('default', ['watch-tsc', 'tsc', 'serve']);
